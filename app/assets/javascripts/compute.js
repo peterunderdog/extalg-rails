05ded5e69d10733212f757049a3af09a04c7a984
Peter Brown <peterunderdog@gmail.com>
2015-06-08 10:25:03 -0400

    fixed blanking grid problem, moved drawGrid method to canvas

--- app/assets/javascripts/compute.js
+++ app/assets/javascripts/compute.js
@@ -1,17 +1,19 @@
 $('canvas.qdraw').ready(function() {
 
+	var theCanvas = $(this);
+
 	var QPoint = function(x, y, label)
 	{
 		this.x = x;
 		this.y = y;
 		this.label = label;
 
-		this.draw = function(canvas)
+		this.draw = function()
 		{
-			$(canvas).drawArc({
+			theCanvas.drawArc({
   		strokeStyle: '#000',
   		strokeWidth: 1,
-  		draggable: true,
+  		draggable: false,
   		fillStyle: 'black',
   		x: x, y: y,
   		radius: 4
@@ -23,60 +25,61 @@ $('canvas.qdraw').ready(function() {
 	{
 		this.qitems = [];
 
-		this.drawGrid = function(canvas) {
-			var GRID_SIZE = 20;
-			var height = $(canvas).height();
-			var width = $(canvas).width();
-
-			var x = GRID_SIZE;
-			var y = GRID_SIZE;
-
-			while (x < width || y < height)
-			{
-				if (x < width)
-				{
-					$(canvas).drawLine({strokeStyle: '#aaa', strokeWidth: 1, 
-						x1: x, y1: 0, x2: x, y2: height});
-					x += GRID_SIZE;
-				}
-				if (y < height)
-				{
-					$(canvas).drawLine({strokeStyle: '#aaa', strokeWidth: 1, 
-						x1: 0, y1: y, x2: width, y2: y});
-					y += GRID_SIZE;
-				}
-			}
-		};
-
 		this.add = function(qitem) {
 			this.qitems.push(qitem);
 		};
 
-		this.draw = function(canvas) {
-			$(canvas).clearCanvas();
+		this.draw = function() {
+			theCanvas.clearCanvas();
 			for (ix in this.qitems) {
-				this.qitems[ix].draw(canvas);
+				this.qitems[ix].draw();
 			}
-			this.drawGrid(canvas);
+			theCanvas.drawGrid();
 		};
-	}
+	};
 
 	var quiver = new Quiver();
 
+	theCanvas.drawGrid = function() {
+		var GRID_SIZE = 20;
+		var height = theCanvas.height();
+		var width = theCanvas.width();
 
-	$('canvas.qdraw').click(function(e){
+		var x = GRID_SIZE;
+		var y = GRID_SIZE;
+
+		while (x < width || y < height)
+		{
+			if (x < width)
+			{
+				theCanvas.drawLine({strokeStyle: '#aaa', strokeWidth: 1, 
+					x1: x, y1: 0, x2: x, y2: height});
+				x += GRID_SIZE;
+			}
+			if (y < height)
+			{
+				theCanvas.drawLine({strokeStyle: '#aaa', strokeWidth: 1, 
+					x1: 0, y1: y, x2: width, y2: y});
+				y += GRID_SIZE;
+			}
+		}
+	};
+
+
+	theCanvas.click(function(e){
 		var x = e.offsetX;
 		var y = e.offsetY;
 
 		var qpoint = new QPoint(x, y, "1");
 		quiver.add(qpoint);
-		quiver.draw(e.target);
+		quiver.draw();
 		}).mouseover(function(e){
 			quiver.draw(e.target);
-		}).mousedown(function(e)
-		{
+		}).mousedown(function(e) {
+			console.log("mousedown");
 			e.preventDefault();
-		});
-
-
+		}).mousemove(function(e) {
+			console.log("mousemove");
+			e.preventDefault();
+		}).drawGrid();
 });
