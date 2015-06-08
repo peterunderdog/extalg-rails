72517976dcde059e4240cecbd0c8e7763d744ee8
Peter Brown <peterunderdog@gmail.com>
2015-06-07 21:21:37 -0400

    object oriented design for Quiver, QItems etc., grid still disappears when mouseover

--- app/assets/javascripts/compute.js
+++ app/assets/javascripts/compute.js
@@ -1,44 +1,76 @@
-$('canvas.qdraw').ready(function(){
+$('canvas.qdraw').ready(function() {
 
-	var drawGrid = function() {
-		var GRID_SIZE = 20;
-		var height = $('canvas').height();
-		var width = $('canvas').width();
+	var QPoint = function(x, y, label)
+	{
+		this.x = x;
+		this.y = y;
+		this.label = label;
 
-		var x = GRID_SIZE;
-		var y = GRID_SIZE;
-
-		while (x < width || y < height)
+		this.draw = function(canvas)
 		{
-			if (x < width)
-			{
-				$('canvas.qdraw').drawLine({strokeStyle: '#aaa', strokeWidth: 1, 
-					x1: x, y1: 0, x2: x, y2: height});
-				x += GRID_SIZE;
-			}
-			if (y < height)
-			{
-				$('canvas.qdraw').drawLine({strokeStyle: '#aaa', strokeWidth: 1, 
-					x1: 0, y1: y, x2: width, y2: y});
-				y += GRID_SIZE;
-			}
-		}
+			$(canvas).drawArc({
+  		strokeStyle: '#000',
+  		strokeWidth: 1,
+  		draggable: true,
+  		fillStyle: 'black',
+  		x: x, y: y,
+  		radius: 4
+			});
+		};
 	};
 
-	drawGrid();
+	var Quiver = function()
+	{
+		this.qitems = [];
+
+		this.drawGrid = function(canvas) {
+			var GRID_SIZE = 20;
+			var height = $(canvas).height();
+			var width = $(canvas).width();
+
+			var x = GRID_SIZE;
+			var y = GRID_SIZE;
+
+			while (x < width || y < height)
+			{
+				if (x < width)
+				{
+					$(canvas).drawLine({strokeStyle: '#aaa', strokeWidth: 1, 
+						x1: x, y1: 0, x2: x, y2: height});
+					x += GRID_SIZE;
+				}
+				if (y < height)
+				{
+					$(canvas).drawLine({strokeStyle: '#aaa', strokeWidth: 1, 
+						x1: 0, y1: y, x2: width, y2: y});
+					y += GRID_SIZE;
+				}
+			}
+		};
+
+		this.add = function(qitem) {
+			this.qitems.push(qitem);
+		};
+
+		this.draw = function(canvas) {
+			this.drawGrid(canvas);
+			for (ix in this.qitems) {
+				this.qitems[ix].draw(canvas);
+			}
+		};
+	}
+
+	var quiver = new Quiver();
+
 
 	$('canvas.qdraw').click(function(e){
 		var x = e.offsetX;
 		var y = e.offsetY;
 
-		$('canvas.qdraw').drawArc({
-  		strokeStyle: '#000',
-  		strokeWidth: 1,
-  		draggable: true,
-  		fillStyle: 'black',
-  		x: e.offsetX, y: e.offsetY,
-  		radius: 4
+		var qpoint = new QPoint(x, y, "1");
+		quiver.add(qpoint);
+		quiver.draw(e.target);
 		});
-	});
+
 
 });
